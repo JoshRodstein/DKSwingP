c5b4d8a58c2de921b54df67dd29f2d22a07d6985
Josh Rodstein <JoshRodstein@users.noreply.github.com>
2018-01-22 17:43:30 -0500

    comments
    added comment descriptions for ENUMS

--- src/column.java
+++ src/column.java
@@ -1,11 +1,11 @@
 public enum column{
 
-    TIMESTAMP,
-    AX,
-    AY,
-    AZ,
-    WX,
-    WY,
-    WZ
+    TIMESTAMP,  
+    AX,  // X-Axis accelerometer
+    AY,  // Y-Axis accelerometer
+    AZ,  // Z-Axis accelerometer
+    WX,  // X-Axis gyroscope
+    WY,  // Y-Axis gyroscope
+    WZ   // Z-Axis gyroscope
 
 }
